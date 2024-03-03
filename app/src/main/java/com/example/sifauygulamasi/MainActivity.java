f6b4035d150b6a65910eeeb0de0a457c3e6823af
Mert Uçan <mertucan12@gmail.com>
2024-03-03 18:42:23 +0300

    added footer buttons

--- app/src/main/java/com/example/sifauygulamasi/MainActivity.java
+++ app/src/main/java/com/example/sifauygulamasi/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
@@ -21,12 +22,18 @@ public class MainActivity extends AppCompatActivity {
     DatabaseHelper db;
 
     Button add_data;
+    Button buttonBitkiler;
+    Button buttonTaslar;
+    Button buttonDualar;
+    Button buttonYaglar;
+    Button buttonCaylar;
     EditText add_name;
 
     ListView plantList;
 
     ArrayList<String> listItem;
     ArrayAdapter adapter;
+    int myColor = Color.parseColor("#4CAF50");
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -41,7 +48,93 @@ public class MainActivity extends AppCompatActivity {
         add_name = findViewById(R.id.add_name);
         plantList = findViewById(R.id.listView);
 
+        buttonBitkiler = findViewById(R.id.buttonBitkiler);
+        buttonTaslar = findViewById(R.id.buttonTaslar);
+        buttonDualar = findViewById(R.id.buttonDualar);
+        buttonYaglar = findViewById(R.id.buttonYaglar);
+        buttonCaylar = findViewById(R.id.buttonCaylar);
+
         viewData();
+
+        buttonBitkiler.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setButtonSelected(buttonBitkiler);
+
+                buttonTaslar.setTextColor(Color.WHITE);
+                buttonTaslar.setBackgroundColor(myColor);
+                buttonCaylar.setTextColor(Color.WHITE);
+                buttonCaylar.setBackgroundColor(myColor);
+                buttonYaglar.setTextColor(Color.WHITE);
+                buttonYaglar.setBackgroundColor(myColor);
+                buttonDualar.setTextColor(Color.WHITE);
+                buttonDualar.setBackgroundColor(myColor);
+            }
+        });
+
+        buttonTaslar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setButtonSelected(buttonTaslar);
+
+                buttonBitkiler.setTextColor(Color.WHITE);
+                buttonBitkiler.setBackgroundColor(myColor);
+                buttonCaylar.setTextColor(Color.WHITE);
+                buttonCaylar.setBackgroundColor(myColor);
+                buttonYaglar.setTextColor(Color.WHITE);
+                buttonYaglar.setBackgroundColor(myColor);
+                buttonDualar.setTextColor(Color.WHITE);
+                buttonDualar.setBackgroundColor(myColor);
+            }
+        });
+
+        buttonDualar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setButtonSelected(buttonDualar);
+
+                buttonTaslar.setTextColor(Color.WHITE);
+                buttonTaslar.setBackgroundColor(myColor);
+                buttonCaylar.setTextColor(Color.WHITE);
+                buttonCaylar.setBackgroundColor(myColor);
+                buttonYaglar.setTextColor(Color.WHITE);
+                buttonYaglar.setBackgroundColor(myColor);
+                buttonBitkiler.setTextColor(Color.WHITE);
+                buttonBitkiler.setBackgroundColor(myColor);
+            }
+        });
+
+        buttonYaglar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setButtonSelected(buttonYaglar);
+
+                buttonTaslar.setTextColor(Color.WHITE);
+                buttonTaslar.setBackgroundColor(myColor);
+                buttonCaylar.setTextColor(Color.WHITE);
+                buttonCaylar.setBackgroundColor(myColor);
+                buttonBitkiler.setTextColor(Color.WHITE);
+                buttonBitkiler.setBackgroundColor(myColor);
+                buttonDualar.setTextColor(Color.WHITE);
+                buttonDualar.setBackgroundColor(myColor);
+            }
+        });
+
+        buttonCaylar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setButtonSelected(buttonCaylar);
+
+                buttonTaslar.setTextColor(Color.WHITE);
+                buttonTaslar.setBackgroundColor(myColor);
+                buttonBitkiler.setTextColor(Color.WHITE);
+                buttonBitkiler.setBackgroundColor(myColor);
+                buttonYaglar.setTextColor(Color.WHITE);
+                buttonYaglar.setBackgroundColor(myColor);
+                buttonDualar.setTextColor(Color.WHITE);
+                buttonDualar.setBackgroundColor(myColor);
+            }
+        });
 
         plantList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -82,5 +175,10 @@ public class MainActivity extends AppCompatActivity {
             adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItem);
             plantList.setAdapter(adapter);
         }
+    }
+
+    private void setButtonSelected(Button button) {
+        button.setTextColor(myColor);
+        button.setBackgroundColor(Color.WHITE);
     }
 }
